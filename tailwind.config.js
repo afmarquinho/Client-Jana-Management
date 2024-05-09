f755ed8b3d5a9f58d09cabe8edd83f60b563c467
Marquinhos <fernandez9029@gmail.com>
2024-05-09 17:22:14 -0500

    se modifica la pagina de login

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        customRed500: "#ff3333",
+        customRed600: "#ee1414",
+        customRed700: "#c80d0d",
+        customRed800: "#c80d0d",
+        customGray: "#d4cdcd"
+      }
+
+    },
   },
   plugins: [],
 }
