31635d2433a67df9d4064b54a2d81ed8d4f1d246
Marquinhos <fernandez9029@gmail.com>
2024-05-17 12:26:24 -0500

    Se completa la eliminacion de informe

--- src/components/VisitReport.tsx
+++ src/components/VisitReport.tsx
@@ -25,6 +25,7 @@ const VisitReport = () => {
   );
 
   const handleReportClick = (itemReport: visitReportTypes) => {
+    console.log(itemReport)
     dispatch(activateViewReport(itemReport));
   };
 
