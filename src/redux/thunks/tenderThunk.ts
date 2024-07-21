bde5bcb8f879aa3e83c853ade4f44489687455d2
Marquinhos <fernandez9029@gmail.com>
2024-07-21 16:56:17 -0500

    Se elimina tenderThunk

--- src/redux/thunks/tenderThunk.ts
+++
@@ -1,9 +0,0 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import { getTendersApi } from "../../services/tenderServices";
-
-
-
-export const fetchTenders = createAsyncThunk("tenders/fetchTenders", async () => {
-  const tenders= await getTendersApi();
-  return tenders
-})
